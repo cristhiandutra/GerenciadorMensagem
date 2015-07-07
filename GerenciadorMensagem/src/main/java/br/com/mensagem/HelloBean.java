1c51494154eb180ab2734dca5feee41028816071
cristhiandutra <cristhiandutra@gmail.com>
2015-07-07 01:14:39 -0300

    Teste

--- GerenciadorMensagem/src/main/java/br/com/mensagem/HelloBean.java
+++ GerenciadorMensagem/src/main/java/br/com/mensagem/HelloBean.java
@@ -3,10 +3,10 @@ package br.com.mensagem;
 import java.io.Serializable;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
 
 @ManagedBean
-@SessionScoped
+@ViewScoped
 public class HelloBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
