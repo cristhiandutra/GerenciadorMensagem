ce964114e93b5595ae9cc3d7dce713b530fc961e
cristhiandutra <cristhiandutra@gmail.com>
2015-07-07 00:22:06 -0300

    Commit do projeto Gerenciador Mensagem

---
+++ GerenciadorMensagem/src/main/java/br/com/mensagem/HelloBean.java
@@ -0,0 +1,23 @@
+package br.com.mensagem;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class HelloBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
