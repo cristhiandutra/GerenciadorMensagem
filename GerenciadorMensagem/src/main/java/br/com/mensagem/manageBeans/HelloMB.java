f589df93ebe526c871176059fc50cd3ebdb0dc08
cristhiandutra <cristhiandutra@gmail.com>
2015-07-15 03:04:09 -0300

    Implementação do CRUD do Usuário.

--- GerenciadorMensagem/src/main/java/br/com/mensagem/manageBeans/HelloMB.java
+++ GerenciadorMensagem/src/main/java/br/com/mensagem/manageBeans/HelloMB.java
@@ -1,17 +1,28 @@
-package br.com.mensagem;
+package br.com.mensagem.manageBeans;
 
 import java.io.Serializable;
 
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
+import br.com.mensagem.ejb.interfaces.HelloLocal;
+
 @ManagedBean
 @ViewScoped
-public class HelloBean implements Serializable {
+public class HelloMB implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@EJB
+	private HelloLocal helloBean;
+	
 	private String nome;
+	
+	public String recuperarNome(){
+		nome = helloBean.recuperarNome();
+		return "welcome";
+	}
 
 	public String getNome() {
 		return nome;
